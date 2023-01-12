4e28c82ba512cfefb8a47144e7190bb414464d90
maria-gianisella <117409544+maria-gianisella@users.noreply.github.com>
2023-01-12 18:32:57 -0300

    Add files via upload

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "729bb0751787a8fb4bcdbbbfe96d3ad6"
+
+let img_url = "https://image.tmdb.org/t/p/w500/"
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
